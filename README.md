fb6a6a59d930e947078e159d6b09f764831130a2
Cappig <prvi.sedmi@gmail.com>
2021-04-14 18:01:55 +0200

    Improvements and changes
    The token is now stored in a .env file, since the bot is hosted on repl.com I added keepalive.js so that uptimerobot.com can keep the bot on. I also shortened the copypasta.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Richard Stallman Bot
 Richard Stallman bot for Discord will remind you to use GNU/Linux and not Linux. 👌
+
+Modified version of this: https://github.com/Technochips/rms-bot
--- index.js
+++ index.js
@@ -1,46 +1,34 @@
-const fs = require('fs')
-const copypasta = require('./copypasta.js');
+require('dotenv').config()
 const Discord = require('discord.js');
+
+const keepAlive = require('./keepalive.js');
+const copypasta = require('./copypasta.js');
+
 const client = new Discord.Client();
 
 //insert code here
 client.on('ready', () =>
 {
 	console.log('Ready');
+	client.user.setActivity('SuperTuxKart', {type: "PLAYING"});
 });
 
 client.on('message', message =>
 {
 	if(message.author.id != client.user.id)
 	{
+
 		if(includesArray(message.cleanContent, copypasta.doesntlike))
 		{
 			if(!includesArray(message.cleanContent, copypasta.likes))
 			{
 				for(var i = 0; i < copypasta.copypasta.length; i++)
 				{
-					message.channel.send(copypasta.copypasta[i]);
+					message.reply(copypasta.copypasta[i]);
 				}
 			}
 		}
-	}
-});
-
-console.log('Reading token file...');
-fs.readFile('./.token', 'utf8', function(err, data)
-{
-	if(err)
-	{
-		console.log('Something bad happened :(');
-		if(err.code = 'ENOENT')
-		{
-			console.log('Couldn\'t read the .token file. Make sure it exists, it should contains your bot\'s token.');
-		}
-		return 1;
-	}
-	console.log('Trying to log-in...');
-	console.log('Using token: ' + data);
-	client.login(data);
+  }
 });
 
 function includesArray(str, arr) //check if the string str contains any element from the array arr
@@ -54,3 +42,6 @@ function includesArray(str, arr) //check if the string str contains any element 
 	}
 	return false;
 }
+
+keepAlive();
+client.login(process.env.TOKEN);
